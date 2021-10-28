ddd23c39a5cf2b2879d4bcb46383b41279c0a4be
Jesse Moniz <jmoniz155@icloud.com>
2021-10-27 19:10:04 -0700

    add instructions

--- develop/models/Category.js
+++ develop/models/Category.js
@@ -5,8 +5,14 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
+  { // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+   
   },
   {
     sequelize,
