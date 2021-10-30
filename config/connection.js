ac5655f17ed2fe214caba9958ddbfced76b784ce
Jesse Moniz <jmoniz155@icloud.com>
2021-10-29 21:17:34 -0700

    Jawsdb config 2

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,21 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize; 
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
     process.env.DB_PW, 
     {
-      host: 'localhost',
+      host: "localhost",
+      dialect: "mysql",
       port: 3306,
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    }
+  );
 }
-
-
-
 
 module.exports = sequelize;
